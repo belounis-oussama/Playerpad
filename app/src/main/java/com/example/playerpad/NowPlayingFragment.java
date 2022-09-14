962e1b69535fd22d3274221387ee9de1a69e0b83
belounis-oussama <contact.elprofessordz@gmail.com>
2022-09-14 20:21:44 +0100

    notifications working

--- app/src/main/java/com/example/playerpad/NowPlayingFragment.java
+++ app/src/main/java/com/example/playerpad/NowPlayingFragment.java
@@ -1,5 +1,6 @@
 package com.example.playerpad;
 
+import static android.content.Context.MODE_PRIVATE;
 import static com.example.playerpad.MainActivity.ARTIST_TO_FRAG;
 import static com.example.playerpad.MainActivity.PATH_TO_FRAG;
 import static com.example.playerpad.MainActivity.SHOW_MINI_PLAYER;
@@ -9,6 +10,7 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.content.SharedPreferences;
 import android.media.Image;
 import android.media.MediaMetadataRetriever;
 import android.os.Bundle;
@@ -33,6 +35,12 @@ public class NowPlayingFragment extends Fragment implements ServiceConnection {
     FloatingActionButton playPauseBtn;
     View view;
     MusicService musicService;
+
+    public static  final  String MUSIC_LAST_PLAYED="LAST_PLAYED";
+    public static final String MUSIC_FILE ="STORED_MUSIC";
+
+    public static  final  String ARTIST_NAME="ARTIST_NAME";
+    public static final String SONG_NAME ="SONG_NAME";
 
 
 
@@ -61,6 +69,59 @@ public class NowPlayingFragment extends Fragment implements ServiceConnection {
                 if (musicService != null)
                 {
                     musicService.nextBtnClicked();
+                    if (getActivity() != null)
+                    {
+
+
+                    SharedPreferences.Editor editor=getActivity().getSharedPreferences(MUSIC_LAST_PLAYED,MODE_PRIVATE).edit();
+
+                    editor.putString(MUSIC_FILE,musicService.musicFiles.get(musicService.position).getPath());
+                    editor.putString(ARTIST_NAME,musicService.musicFiles.get(musicService.position).getArtist());
+                    editor.putString(SONG_NAME,musicService.musicFiles.get(musicService.position).getTitle());
+                    editor.apply();
+                        SharedPreferences preferences=getActivity().getSharedPreferences(MUSIC_LAST_PLAYED,MODE_PRIVATE);
+                        String path =preferences.getString(MUSIC_FILE,null);
+                        String artist =preferences.getString(ARTIST_NAME,null);
+                        String song_name =preferences.getString(SONG_NAME,null);
+
+                        if (path != null)
+                        {
+                            SHOW_MINI_PLAYER= true;
+                            PATH_TO_FRAG =path;
+                            ARTIST_TO_FRAG = artist;
+                            SONG_NAME_TO_FRAG = song_name;
+                        }
+                        else
+                        {
+                            SHOW_MINI_PLAYER=false;
+                            PATH_TO_FRAG=null;
+                            ARTIST_TO_FRAG = null;
+                            SONG_NAME_TO_FRAG = null;
+                        }
+                        if (SHOW_MINI_PLAYER)
+                        {
+                            if (PATH_TO_FRAG != null)
+                            {
+                                byte[] art=getAlbumArt(PATH_TO_FRAG);
+
+                                if (art != null)
+                                {
+                                    Glide.with(getContext()).load(art).into(albumArt);
+                                }
+                                else
+                                {
+                                    albumArt.setImageResource(R.drawable.ic_round_music_note_24);
+                                }
+
+                                songname.setText(SONG_NAME_TO_FRAG);
+                                artistname.setText(ARTIST_TO_FRAG);
+
+
+                            }
+
+                        }
+                    }
+
                 }
             }
         });
